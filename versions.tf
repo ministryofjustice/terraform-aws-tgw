80b3d94aca4009129f201221d3e0c0ee952c44ee
Andy Cohen <andrew.cohen1@justice.gov.uk>
2022-04-27 14:26:39 +0100

    Add provider configuration_alias

--- versions.tf
+++ versions.tf
@@ -2,6 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "registry.terraform.io/hashicorp/aws"
+      configuration_aliases = [ aws.ireland ]
+
     }
   }
   required_version = ">= 0.13"
