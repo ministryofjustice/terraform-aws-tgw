4ea116ccce232dc16965c79ad1014cfd49771513
Andy Cohen <andrew.cohen1@justice.gov.uk>
2022-04-27 13:29:05 +0100

    Change provider input

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_ec2_transit_gateway" "this" {
 
   tags = merge(var.tags, { Name = each.key })
 
-  provider = lookup(each.value, "provider", "aws")
+  provider = lookup(each.value, "provider", "aws.london")
 }
 
 
@@ -34,5 +34,5 @@ resource "aws_ec2_transit_gateway_route_table" "this" {
 
   tags = merge(var.tags, { Name = each.key })
 
-  provider = lookup(each.value, "provider", "aws")
+  provider = lookup(each.value, "provider", "aws.london")
 }
